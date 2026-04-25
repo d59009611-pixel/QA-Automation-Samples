f248b1359c0d6694788d02a2e451728bb70ea37b
d59009611-pixel <d59009611@gmail.com>
2026-04-25 11:17:28 +0500

    Update UserValidator.java

--- UserValidator.java
+++ UserValidator.java
@@ -1,1 +1,9 @@
+public class UserValidator {
+    public boolean isSecurePassword(String password) {
+        return password != null && password.length() >= 8;
+    }
+    public boolean isValidEmail(String email) {
+        return email != null && email.contains("@") && email.contains(".");
+    }
+}
 
