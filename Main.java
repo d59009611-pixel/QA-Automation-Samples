5a2958e9b083fe2f7389c3d72bddaa154d7cfc02
d59009611-pixel <d59009611@gmail.com>
2026-04-25 11:57:56 +0500

    Update validation checks in Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         UserValidator validator = new UserValidator();
-        System.out.println("Проверка номера +998... : " + validator.checkPhone("+998997336273"));
-        System.out.println("Проверка короткого пароля: " + validator.checkPassword("123"));
-        System.out.println("Проверка имени: " + validator.checkName("Daler"));
+
+        System.out.println("Phone: " + validator.checkPhone("+998 (99) 733-62-73"));
+        System.out.println("Name with digits: " + validator.checkName("Daler123"));
+        System.out.println("Pass: " + validator.checkPassword("password123"));
     }
 }
