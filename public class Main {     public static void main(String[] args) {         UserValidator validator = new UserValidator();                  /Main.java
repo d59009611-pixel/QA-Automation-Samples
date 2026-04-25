81fcf3cd09ec45a5c8d865ec1e9e043796c1b464
d59009611-pixel <d59009611@gmail.com>
2026-04-25 11:33:46 +0500

    Delete public class Main {     public static void main(String[] args) {         UserValidator validator = new UserValidator();                  /Main.java

--- public class Main {     public static void main(String[] args) {         UserValidator validator = new UserValidator();                  /Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        UserValidator validator = new UserValidator();
-        
-        System.out.println("Проверка номера +998... : " + validator.checkPhone("+998997336273"));
-        System.out.println("Проверка короткого пароля: " + validator.checkPassword("123"));
-        System.out.println("Проверка имени: " + validator.checkName("Daler"));
-    }
-}
