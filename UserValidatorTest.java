54b68935e422ace71fb3e1361c7c1564dee40edb
d59009611-pixel <d59009611@gmail.com>
2026-04-25 11:18:35 +0500

    Update UserValidatorTest.java

--- UserValidatorTest.java
+++ UserValidatorTest.java
@@ -1,1 +1,19 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserValidatorTest {
+    UserValidator validator = new UserValidator();
+
+    @Test
+    void testPassword() {
+        assertTrue(validator.isSecurePassword("mySafePassword123"));
+        assertFalse(validator.isSecurePassword("123"));
+    }
+
+    @Test
+    void testEmail() {
+        assertTrue(validator.isValidEmail("daler@google.com"));
+        assertFalse(validator.isValidEmail("daler.com"));
+    }
+}
 
