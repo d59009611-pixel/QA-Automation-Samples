a20214523faae98710b6262f6d93aecc21e0873f
d59009611-pixel <d59009611@gmail.com>
2026-04-25 11:32:59 +0500

    Create Main.java

---
+++ public class Main {     public static void main(String[] args) {         UserValidator validator = new UserValidator();                  /Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        UserValidator validator = new UserValidator();
+        
+        System.out.println("Проверка номера +998... : " + validator.checkPhone("+998997336273"));
+        System.out.println("Проверка короткого пароля: " + validator.checkPassword("123"));
+        System.out.println("Проверка имени: " + validator.checkName("Daler"));
+    }
+}
