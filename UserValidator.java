02e2378b91c9e9bdf04191de5cb6020f6d4b339f
d59009611-pixel <d59009611@gmail.com>
2026-04-25 11:22:05 +0500

    Update UserValidator.java

--- UserValidator.java
+++ UserValidator.java
@@ -1,9 +1,16 @@
 public class UserValidator {
+
+    public boolean isValidPhoneNumber(String phone) {
+        if (phone == null) return false;
+        String cleanPhone = phone.trim();
+        return cleanPhone.startsWith("+998") && cleanPhone.length() == 13;
+    }
+
+    public boolean isValidName(String name) {
+        return name != null && !name.trim().isEmpty() && name.matches("^[a-zA-Zа-яА-Я]*$");
+    }
+
     public boolean isSecurePassword(String password) {
         return password != null && password.length() >= 8;
     }
-    public boolean isValidEmail(String email) {
-        return email != null && email.contains("@") && email.contains(".");
-    }
 }
-
